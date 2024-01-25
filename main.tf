c3a775f83629fdeb4c7947db5065e28da55a4229
Leonid Frolov <51460118+leonidfrolov@users.noreply.github.com>
2024-01-25 10:24:29 +0200

    fix: updated vmss module version

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "random_password" "this" {
 
 module "vmss" {
   source  = "data-platform-hq/vmss/azurerm"
-  version = "1.2.0"
+  version = "1.2.1"
 
   tags                     = var.tags
   resource_group           = var.resource_group
